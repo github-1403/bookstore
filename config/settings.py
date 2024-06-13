7a3290d05fa863e0d18b0c1ca0be268a0fdb791f
github-1403 <fllstck.shp@gmail.com>
2024-06-13 23:56:08 +0330

    add crispy forms

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,10 @@ INSTALLED_APPS = [
     'accounts',
     'pages',
 
+    #
+    'crispy_forms',
+    'crispy_bootstrap5',
+
 ]
 
 MIDDLEWARE = [
@@ -132,3 +136,8 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+
+# crispy forms setting
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
